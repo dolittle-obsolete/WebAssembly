cc96993ff7b826af153fa5f6fb5d09889f2bf92a
Einar Ingebrigtsen <einar@dolittle.com>
2018-11-07 15:42:49 +0100

    Simple testing

--- Samples/Basic/Program.cs
+++ Samples/Basic/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Dolittle.Hosting;
+using WebAssembly;
 
 namespace Basic
 {
@@ -6,7 +8,14 @@ namespace Basic
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("HELLO WORLD 2");
+
+            var window = (JSObject)WebAssembly.Runtime.GetGlobalObject("window");
+            var location = (JSObject)window.GetObjectProperty("location");
+
+            Console.WriteLine("Location : "+location);
+            //var hostBuilder = new HostBuilder();
+            //hostBuilder.Build();
         }
     }
 }
