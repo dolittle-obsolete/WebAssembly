d5faae9394dbcdbeefbfeb6f06d1bb756359d74c
Einar Ingebrigtsen <einar@dolittle.com>
2019-01-09 21:32:10 +0100

    Runtime hookup up of mono + service worker

--- Source/Client.Aurelia/index.js
+++ Source/Client.Aurelia/index.js
@@ -7,7 +7,43 @@ import { CommandCoordinator as WASMCommandCoordinator } from '@dolittle/webassem
 import { QueryCoordinator } from '@dolittle/queries';
 import { QueryCoordinator as WASMQueryCoordinator } from '@dolittle/webassembly/queries';
 
-export function configure(aurelia) {
+
+export function configure(aurelia, config) {
     aurelia.container.registerInstance(CommandCoordinator, new WASMCommandCoordinator());
     aurelia.container.registerInstance(QueryCoordinator, new WASMQueryCoordinator());
+
+    config = config || {};
+    config.entryPoint = config.entryPoint || '';
+    config.assemblies = config.assemblies || [];
+    config.monoScript = config.monoScript || 'mono.js';
+    config.offline = config.offline || false;
+
+    if (config.entryPoint == '') throw "Missing entrypoint in Dolittle WebAssembly plugin configuration";
+    if (config.assemblies.length == 0) throw "Missing assemblies in Dolittle WebAssembly plugin configuration";
+
+    console.log(`Using '${config.entryPoint}' as entrypoint for WebAssembly `)
+
+    window.Module = {};
+
+    window.Module.onRuntimeInitialized = () => {
+        MONO.mono_load_runtime_and_bcl(
+            'managed',
+            'managed',
+            1,
+            config.assemblies,
+            () => {
+                Module.mono_bindings_init("[WebAssembly.Bindings]WebAssembly.Runtime");
+                BINDING.call_static_method(config.entryPoint, []);
+            }
+        );
+    };
+
+    let monoScript = document.createElement('script');
+    monoScript.async = true;
+    monoScript.src = config.monoScript;
+    document.body.appendChild(monoScript);
+
+    if (config.offline === true) {
+        navigator.serviceWorker.register('service-worker.js');
+    }
 }
