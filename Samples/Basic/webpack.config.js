7b0f15fbf1a14dfaf24f09cc9e4bab3e9a03bc69
Einar Ingebrigtsen <einar@dolittle.com>
2019-01-11 18:54:48 +0100

    Getting it all together for the client files needed

--- Samples/Basic/webpack.config.js
+++ Samples/Basic/webpack.config.js
@@ -1,17 +1,22 @@
 const config = require('@dolittle/build.aurelia/webpack.config.js')();
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ServiceWorkerGenerator = require('@dolittle/webassembly.webpack/ServiceWorkerGenerator');
 
 config.entry = {
-    app: ['babel-polyfill', 'aurelia-bootstrapper'],
+    app: ['@babel/polyfill', 'aurelia-bootstrapper'],
     vendor: ['bluebird']
 };
 
 config.plugins.push(new CopyWebpackPlugin([
     { from: 'publish/managed/**/*', to: 'managed', flatten: true },
     { from: 'publish/mono.js', to: 'mono.js', flatten: true },
-    { from: 'publish/mono-config.js', to: 'mono-config.js', flatten: true },
-    { from: 'publish/runtime.js', to: 'runtime.js', flatten: true },
-    { from: 'publish/mono.wasm', to: 'mono.wasm', flatten: true }
+    { from: 'publish/mono.wasm', to: 'mono.wasm', flatten: true },
+    { from: 'manifest.json', to: 'manifest.json', flatten: true },
+    { from: 'dolittle.png', to: 'dolittle.png', flatten: true },
+    { from: 'sqlite3.wasm', to: 'sqlite3.wasm', flatten: true },
+    { from: 'sqlite3.js', to: 'sqlite3.js', flatten: true }
 ]));
+
+config.plugins.push(new ServiceWorkerGenerator());
 
 module.exports = config;
