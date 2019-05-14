dbf628a42ccb8ddc24922fbdea2752c128aca52a
Einar Ingebrigtsen <einar@dolittle.com>
2019-05-14 22:58:09 +0200

    Deploy version 3.1000 for local

--- DeployPackagesLocally.sh
+++ DeployPackagesLocally.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export PACKAGEDIR=$PWD/Packages
-export PACKAGEVERSION=2.1000.0
+export PACKAGEVERSION=3.1000.0
 export TARGETROOT=~/.nuget/packages
 
 if [ ! -d "$PACKAGEDIR" ]; then
@@ -11,11 +11,11 @@ rm $PACKAGEDIR/*
 dotnet pack ./Source/Core -p:PackageVersion=$PACKAGEVERSION --include-symbols --include-source -o $PACKAGEDIR
 find $TARGETROOT/Dolittle.Interaction.WebAssembly.Core -name $PACKAGEVERSION -exec rm -rf {} \;
 target=$TARGETROOT/Dolittle.Interaction.WebAssembly.Core/$PACKAGEVERSION
-mkdir -pv $target && cp -v $PACKAGEDIR/Dolittle.Interaction.WebAssembly.Core.2.1000.0.nupkg $target
+mkdir -pv $target && cp -v $PACKAGEDIR/Dolittle.Interaction.WebAssembly.Core.3.1000.0.nupkg $target
 # Unzip package
-tar -xzf $PACKAGEDIR/Dolittle.Interaction.WebAssembly.Core.2.1000.0.nupkg -C $target
+tar -xzf $PACKAGEDIR/Dolittle.Interaction.WebAssembly.Core.3.1000.0.nupkg -C $target
 # Create an empty .sha512 file, or else it won't recognize the package for some reason
-touch $target/Dolittle.Interaction.WebAssembly.Core.2.1000.0.nupkg.sha512
+touch $target/Dolittle.Interaction.WebAssembly.Core.3.1000.0.nupkg.sha512
 
 dotnet pack -p:PackageVersion=$PACKAGEVERSION --include-symbols --include-source -o $PACKAGEDIR
 
@@ -25,7 +25,7 @@ done
 
 for f in $PACKAGEDIR/*.nupkg; do
     echo ""
-    packagename=$(basename ${f%.2.1000.0.nupkg})
+    packagename=$(basename ${f%.3.1000.0.nupkg})
     target=$TARGETROOT/$packagename/$PACKAGEVERSION
     # Delete outdated .nupkg 
     find $TARGETROOT/$packagename -name $PACKAGEVERSION -exec rm -rf {} \;
