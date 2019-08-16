ddaacb4d782bc997587c8eec7635ad4eaba1904c
Einar Ingebrigtsen <einar@dolittle.com>
2019-08-16 17:53:35 +0200

    Removing whitespace

--- Source/Client.Interop/DotNetRuntime.js
+++ Source/Client.Interop/DotNetRuntime.js
@@ -1,4 +1,3 @@
-
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Dolittle. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
