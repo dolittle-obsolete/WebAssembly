5c3486c28aec5e3bed3822b83337c9feb3b96bb7
Einar Ingebrigtsen <einar@dolittle.com>
2019-08-16 17:53:19 +0200

    Removing whitespace

--- Source/Client.Interop/JSRuntime.js
+++ Source/Client.Interop/JSRuntime.js
@@ -1,5 +1,3 @@
-import { invoker } from "aurelia-framework";
-
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Dolittle. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
